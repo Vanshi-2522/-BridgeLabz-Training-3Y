cf691a476b4ea44292288478dbf2957acb1bec60
Vanshika Gupta <vanshi22gupta@gmail.com>
2025-08-14 15:52:28 +0530

    Create Side of Square from Perimeter.java

---
+++ level2/Side of Square from Perimeter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class SquareSide {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double perimeter, side;
+
+        System.out.print("Enter perimeter: ");
+        perimeter = input.nextDouble();
+
+        side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
