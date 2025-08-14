4c860802031bcb680ce3706dd39f5bc2b93833dd
Vanshika Gupta <vanshi22gupta@gmail.com>
2025-08-14 15:33:39 +0530

    Average Marks in PCM.java

---
+++ level 1/Average Marks in PCM.java
@@ -0,0 +1,13 @@
+class SamPCMPercentage {
+    public static void main(String[] args) {
+
+        // Marks for each subject
+        int mathsMarks = 94;
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+
+        // Calculate average
+        double averageMarks = (mathsMarks + physicsMarks + chemistryMarks) / 3.0;
+
+        // Display output
+        System.out.println("Sam’s average mark in PCM is " + averageMarks);
