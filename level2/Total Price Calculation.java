ce9b18ac96f672003646fd4c5479f7a51c654141
Vanshika Gupta <vanshi22gupta@gmail.com>
2025-08-14 15:53:40 +0530

    Create Total Price Calculation.java

---
+++ level2/Total Price Calculation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class TotalPurchasePrice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double unitPrice, totalPrice;
+        int quantity;
+
+        System.out.print("Enter unit price: ");
+        unitPrice = input.nextDouble();
+
+        System.out.print("Enter quantity: ");
+        quantity = input.nextInt();
+
+        totalPrice = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR " + totalPrice 
+                           + " if the quantity " + quantity + 
+                           " and unit price is INR " + unitPrice);
+    }
+}
